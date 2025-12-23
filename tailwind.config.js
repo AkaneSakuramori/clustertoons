96556c8f5c15b7afc1762bf370a00d1304fd6ab4
AkaneSakuramori <utkarshffuff@gmail.com>
2025-12-23 15:45:36 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        bg: "#0b0e13",
+        card: "#11151c",
+        accent: "#e50914"
+      }
+    }
+  },
+  plugins: []
+};
